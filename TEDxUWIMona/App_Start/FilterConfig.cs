6267ea1c3f30bb6a6f774fadbd8d77300022a909
kerette adams <kerette_111@yahoo.com>
2018-07-12 23:32:58 -0500

    Initial commit

---
+++ TEDxUWIMona/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TEDxUWIMona
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TEDxUWIMona/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEDxUWIMona.Startup))]
+namespace TEDxUWIMona
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
