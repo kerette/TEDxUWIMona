e6c6d9fe551f7a1b6c5e4a1c6076c7c26c07e4ea
kerette adams <kerette_111@yahoo.com>
2018-07-29 23:10:39 -0500

    modified bundle to work when deployed

--- TEDxUWIMona/App_Start/BundleConfig.cs
+++ TEDxUWIMona/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace TEDxUWIMona
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                 "~/Content/font-awesome.css"));
